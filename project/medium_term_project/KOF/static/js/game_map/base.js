1735a5d4cc1797491f7938c0a41c2cb35ffb26f2
lyn <liuyunuo1018@163.com>
2022-07-21 10:33:24 +0800

    finish controller add update_control

--- project/medium_term_project/KOF/static/js/game_map/base.js
+++ project/medium_term_project/KOF/static/js/game_map/base.js
@@ -1,15 +1,17 @@
 import { SupGameObject } from "../sup_game_object/base.js";
+import { Controller } from '../controller/base.js';
 
 export class GameMap extends SupGameObject {
     constructor(root) {
         super();
 
         this.root = root;
-        this.$canvas = $('<canvas width="1120" height="630" tabindex=0></canvas>')
+        this.$canvas = $('<canvas width="1280" height="720" tabindex=0></canvas>')
         this.ctx = this.$canvas[0].getContext('2d');
         this.root.$kof.append(this.$canvas);
         this.$canvas.focus();
 
+        this.controller = new Controller(this.$canvas);
     }
 
     start() {
